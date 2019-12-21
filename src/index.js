df0ffb49958871b4cf7c6cd77e8ec6ff4e2e533a
Pablo Cardozo <pablocyc83@gmail.com>
2019-12-21 16:21:12 -0400

    controlando led rgb con web-page & websockets

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const jf = require('johnny-five')
+const WebSocket = require('ws')
+const path = require('path');
 const port = 3000
 
+const wss = new WebSocket.Server({port: 3001})
 const app = express()
 const circuito = new jf.Board()
 
@@ -10,11 +13,17 @@ circuito.on('ready', prender)
 function prender () {
   let rgb = new jf.Led.RGB([10, 11, 9])
   rgb.color('#ff00ff') 
+
+  wss.on('connection', function (ws, req) {
+    console.log('Conectado...')
+    ws.on('message', function (data) {
+      console.log(data)
+      rgb.color(data)
+    })
+  })
 }
 
-app.get('/', function (req, res) {
-  res.send('Hola mundo')
-})
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.listen(port)
 console.log(`Servidor escuchando en http://localhost:${port}`)
