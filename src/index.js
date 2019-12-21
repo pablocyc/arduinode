e722ecd0fcbff9fa65b3a7ed9ec3e8ca5fc9bef5
Pablo Cardozo <pablocyc83@gmail.com>
2019-12-21 14:42:47 -0400

    Levantando el servidor con express

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
+const express = require('express')
 const jf = require('johnny-five')
+const port = 3000
+
+const app = express()
 const circuito = new jf.Board()
 
 circuito.on('ready', prender)
@@ -7,3 +11,11 @@ function prender () {
   let rgb = new jf.Led.RGB([10, 11, 9])
   rgb.color('#ff00ff') 
 }
+
+app.get('/', function (req, res) {
+  res.send('Hola mundo')
+})
+
+app.listen(port)
+console.log(`Servidor escuchando en http://localhost:${port}`)
+
